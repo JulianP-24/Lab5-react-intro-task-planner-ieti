d199157875e3b8d4df385ce22b53d264bddfbe03
Julian Peña <julian.pena-m@mail.escuelaing.edu.co>
2022-09-22 18:37:35 -0500

    Material icon en la vista de login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,44 +1,93 @@
 import React, { useState } from "react";
+import { Grid, Paper, Avatar, TextField } from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import UserIcon from "@mui/icons-material/Person";
+import PasswordIcon from "@mui/icons-material/Password";
+import ListItemIcon from "@mui/material/ListItemIcon";
 
 function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const paperStyle = {
+    padding: 20,
+    height: "70vh",
+    width: 310,
+    margin: "20px auto",
+  };
+  const avatarStyle = { backgroundColor: "#1bbd7e" };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    function handleEmail (evt)  {
-      setEmail(evt.target.value);
-    }
-
-    function handlePassword (evt) {
-        setPassword(evt.target.value);
-    }
-
-    function handleLogin (evt)  {
-    }
-
-    return (
-      <div className="wrapper fadeInDown">
-        <div id="formContent">
-
-          <form onSubmit={handleLogin}>
-            <input
-              type="text"
-              className="fadeIn second"
-              name="email"
-              placeholder="Email"
-              onChange={handleEmail}
-            />
-            <input
-              type="password"
-              className="fadeIn third"
-              name="password"
-              placeholder="password"
-              onChange={handlePassword}
-            />
-            <input type="submit" className="fadeIn fourth" value="Log In" />
-          </form>
-        </div>
-      </div>
-    );
+  function handleEmail(evt) {
+    setEmail(evt.target.value);
   }
+
+  function handlePassword(evt) {
+    setPassword(evt.target.value);
+  }
+
+  function handleLogin(evt) {}
+
+  return (
+    <Grid>
+      <form onSubmit={handleLogin} className="">
+        <Paper elevation={10} style={paperStyle} className="card">
+          <Grid align="center">
+            <Avatar style={avatarStyle}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <h2>Iniciar Sesion</h2>
+          </Grid>
+          <div className="text login">
+            <div>
+              <ListItemIcon>
+                <UserIcon className="icons" />{" "}
+              </ListItemIcon>
+              <TextField
+                variant="outlined"
+                id="user"
+                name="user"
+                label="Username"
+                type="email"
+                value={email?.email}
+                onChange={handleEmail}
+              />
+            </div>
+            <br></br>
+            <div>
+              <ListItemIcon>
+                <PasswordIcon className="icons" />{" "}
+              </ListItemIcon>
+              <FormControl className="" variant="outlined">
+                <InputLabel
+                  htmlFor="outlined-adornment-password"
+                  value={password}
+                >
+                  Password
+                </InputLabel>
+                <OutlinedInput
+                  fullWidth
+                  label="Password"
+                  id="outlined-adornment-password-login"
+                  type="password"
+                  name="password"
+                  autoComplete="off"
+                  onChange={handlePassword}
+                />
+              </FormControl>
+            </div>
+          </div>
+          <br />
+          <br />
+          <br />
+          <button className="boton" type="submit" variant="contained">
+            Sign in
+          </button>
+        </Paper>
+      </form>
+    </Grid>
+  );
+}
 
 export default Login;
